8c2b859ffec71527af19d4c525788ec74ac4a62a
LonelyBush23 <66avasvs@gmail.com>
2025-01-02 19:44:50 +0500

    CommonRMQ

---
+++ Common.RabbitMQ/IRabbitMQService.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+
+namespace Common.RabbitMQ
+{
+    public interface IRabbitMQService
+    {
+        Task<IChannel> GetChannelAsync(CancellationToken cancellationToken = default);
+
+        Task DeclareQueueAsync(string queue, IChannel channel, CancellationToken cancellationToken = default);
+
+        Task SubscribeToQueueAsync(string queue, Func<string, Task> messageHandler, CancellationToken cancellationToken = default);
+
+        Task DisposeAsync();
+    }
+}
---
+++ Common.RabbitMQ/RabbitMQService.cs
@@ -0,0 +1,101 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Common.RabbitMQ
+{
+    public class RabbitMQService : IRabbitMQService
+    {
+        private ConnectionFactory _factory;
+        private IConnection? _connection;
+        private IChannel? _channel;
+
+        public RabbitMQService()
+        {
+            _factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "admin"
+            };
+        }
+
+        public async Task DeclareQueueAsync(string queue, IChannel channel, CancellationToken cancellationToken = default)
+        {
+            await channel.QueueDeclareAsync(queue: queue,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null,
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<IChannel> GetChannelAsync(CancellationToken cancellationToken = default)
+        {
+            if (_connection == null || !_connection.IsOpen)
+            {
+                _connection = await _factory.CreateConnectionAsync(cancellationToken);
+            }
+
+            if (_channel == null || _channel.IsClosed)
+            {
+                _channel = await _connection.CreateChannelAsync();
+            }
+            return _channel;
+        }
+
+        public async Task SubscribeToQueueAsync(string queue, Func<string, Task> messageHandler, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var channel = await GetChannelAsync();
+                var consumer = new AsyncEventingBasicConsumer(channel);
+
+                consumer.ReceivedAsync += async (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(message);
+                    try
+                    {
+                        await messageHandler(message);
+                        // сохранение в БД об успехе
+                        await channel.BasicAckAsync(ea.DeliveryTag, false);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error processing message: {ex.Message}");
+                        // сохранение в БД об неудаче
+                        // Optionally, you can use BasicNack to requeue or discard the message
+                        await channel.BasicNackAsync(ea.DeliveryTag, false, true);
+                    }
+                };
+
+                await channel.BasicConsumeAsync(queue: queue, autoAck: false, consumer: consumer);
+
+                Console.WriteLine($"Listening on queue: {queue}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error initializing consumer: {ex.Message}");
+            }
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (_channel != null && _channel.IsOpen)
+            {
+                await _channel.CloseAsync();
+                _channel.Dispose();
+                _channel = null;
+            }
+
+            if (_connection != null && _connection.IsOpen)
+            {
+                await _connection.CloseAsync();
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
